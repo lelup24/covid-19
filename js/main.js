77d775e6dc33846891a476a83508e44c313e037c
Nigel Dcruz <nigeldcruz7@gmail.com>
2020-03-28 17:48:11 +0530

    Async Await added

--- js/main.js
+++ js/main.js
@@ -1,7 +1,15 @@
-fetch('https://corona.lmao.ninja/all')
-  .then((response) => {
-    return response.json();
-  })
-  .then((data) => {
-    console.log(data);
-  });
+// Gets All global cases
+(async () => {
+  let data = await fetch("https://corona.lmao.ninja/all");
+
+  let all = await data.json();
+
+  let allCases = all.cases;
+  let allDeaths = all.deaths;
+  let allRecovered = all.recovered;
+
+  console.log(allCases);
+  console.log(allDeaths);
+  console.log(allRecovered);
+  
+})();
